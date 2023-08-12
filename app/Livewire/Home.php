a6f3d8f5b6242b68566b46d4719db55026019594
Mart <m@rtin.win>
2023-08-12 07:59:43 +0200

    Removed test button from home

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,21 +2,8 @@
 
 namespace App\Livewire;
 
-use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
-use Native\Laravel\Dialog;
-use Native\Laravel\Facades\Notification;
 
 class Home extends Component
 {
-    public function test()
-    {
-        // TODO: Fix the disks for native.
-        dd(Storage::disk('documents')->files());
-        $folder = Dialog::new()->folders()->open();
-        dd($folder);
-        // Notification::title('Hello from NativePHP')
-        //     ->message('This is a detail message coming from your Laravel app.')
-        //     ->show();
-    }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,6 +2,5 @@
     <x-sections.card>
         <h1 class="text-4xl">Landing Page</h1>
         <p>Welcome to a brand new idea!</p>
-        <x-buttons.main wire:click="test()">Test noti</x-buttons.main>
     </x-sections.card>
 </x-layouts.container>
